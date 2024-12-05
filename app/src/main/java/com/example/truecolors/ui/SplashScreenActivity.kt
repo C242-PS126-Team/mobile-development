8a3a2f861e2f585d79a955d55706faf37f311dfd
GinaFerl <ginaferlindawati@gmail.com>
2024-12-06 00:03:10 +0700

    fix bug

--- app/src/main/java/com/example/truecolors/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/example/truecolors/ui/SplashScreenActivity.kt
@@ -1,5 +1,6 @@
 package com.example.truecolors.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -11,6 +12,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.truecolors.R
 
+@SuppressLint("CustomSplashScreen")
+@Suppress("DEPRECATION")
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/truecolors/ui/test/ColorBlindTestFragment.kt
+++ app/src/main/java/com/example/truecolors/ui/test/ColorBlindTestFragment.kt
@@ -5,14 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.ActionBar
 import androidx.fragment.app.Fragment
-import com.example.truecolors.MainActivity
-import com.example.truecolors.R
 import com.example.truecolors.data.test.TestConstant
 import com.example.truecolors.databinding.FragmentColorBlindTestBinding
+import com.example.truecolors.ui.MainActivity
 
 class ColorBlindTestFragment : Fragment() {
 
